5494b6e63e26a0faf4127735b4d07110fa9ffe26
amfzn503 <amfzn503@msn.com>
2020-05-20 10:08:18 -0500

    part 6 complete

---
+++ src/org/launchcode/techjobs_oo/JobField.java
@@ -0,0 +1,40 @@
+package org.launchcode.techjobs_oo;
+
+import java.util.Objects;
+
+public abstract class JobField {
+    private int id;
+    private static int nextId = 1;
+    private String value;
+
+    public JobField () {
+        id = nextId;
+        nextId++;
+    }
+    public JobField (String value){
+        this();
+        this.value = value;
+    }
+
+    public String toString() {
+        return value;
+    }
+
+    public int hashCode() {
+        return Objects.hash(getId());
+    }
+
+    //Getters and setters
+    public int getId() {
+        return id;
+    }
+    public static int getNextId() {
+        return nextId;
+    }
+    public String getValue() {
+        return value;
+    }
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
